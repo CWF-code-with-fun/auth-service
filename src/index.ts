28a186ec77c4f23f079903ffdf2d267afb9a90ed
DESKTOP-HBI9H3K\Kuntal <kuntal.ghosh@bjitgroup.com>
2023-11-03 21:09:55 +0600

    added configuration

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
+import { Config } from './Config';
+
 function login(userName: string) {
     return userName;
 }
 
 login('Kuntal');
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const port = Config.PORT;
