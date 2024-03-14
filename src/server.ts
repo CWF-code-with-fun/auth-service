cd212de006a1328d1d8da0d1e441cbcc4a25d52e
Kuntal Ghosh <kuntal.ghosh@bjitgroup.com>
2024-03-14 15:48:20 +0600

    feat:configured winston in config

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import app from './app';
-
+import logger from './Config/logger'
 import { Config } from './Config';
 
 const startServer = () => {
     const port = Config.PORT;
 
     try {
-        app.listen(port, () => console.log("listening on port " + port));
-    } catch (err) {
+        app.listen(port, () => logger.info("listening on port "+ port));
+    } catch (err:unknown) {
+        if(err instanceof Error){
+            logger.error(err.message)
+        }
         process.exit(1);
     }
 };
