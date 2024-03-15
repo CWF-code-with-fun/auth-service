111ec5852ed3bc416fcfede238e926febc45a801
Kuntal Ghosh <kuntal.ghosh@bjitgroup.com>
2024-03-15 15:23:45 +0600

    feat:added full template

--- src/Config/index.ts
+++ src/Config/index.ts
@@ -2,11 +2,11 @@ import { config } from 'dotenv';
 
 config({
     path: '.env',
-});
+}); 
 
 const { PORT, NODE_ENV } = process.env;
-
+ 
 export const Config = {
     PORT,
     NODE_ENV,
-};
+}; 
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,15 @@
 import app from './app';
-import logger from './Config/logger'
+import logger from './Config/logger';
 import { Config } from './Config';
 
 const startServer = () => {
-    const port = Config.PORT;
+    const port = Config.PORT; 
 
     try {
-        app.listen(port, () => logger.info("listening on port "+ port));
-    } catch (err:unknown) {
-        if(err instanceof Error){
-            logger.error(err.message)
+        app.listen(port, () => logger.info('listening on port ' + port));
+    } catch (err: unknown) {
+        if (err instanceof Error) {
+            logger.error(err.message);
         }
         process.exit(1);
     }
